c07e68e5d6533b6359aaad74910fc0478756993a
CosmoBot <vitaliy.brooks@gmail.com>
2021-03-06 21:18:23 +0700

    Add color theme in page constructor

---
+++ static/access/scripts/widgets.js
@@ -0,0 +1,22 @@
+document.getElementById("open_widget_messenger").onclick = function() {
+    return document.getElementById("widget_messenger").style.display = "block";
+}
+document.getElementById("close_widget_messenger").onclick = function() {
+    return document.getElementById("widget_messenger").style.display = "none";
+}
+
+// Plugin
+document.getElementById("open_widget_plugin").onclick = function() {
+    return document.getElementById("widget_plugin").style.display = "block";
+}
+document.getElementById("close_widget_plugin").onclick = function() {
+    return document.getElementById("widget_plugin").style.display = "none";
+}
+
+// Success
+document.getElementById("success").onclick = function() {
+    return document.getElementById("widget_success").style.display = "block";
+}
+document.getElementById("close_success").onclick = function() {
+    return document.getElementById("widget_success").style.display = "none";
+}
